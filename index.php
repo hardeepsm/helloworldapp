746122b8d847fb3999d6f3de7046dd2814cfecdd
hardeepsm <48568440+hardeepsm@users.noreply.github.com>
2019-06-27 15:39:58 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo 'Hello New World running on -'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'];
+echo 'Hello World Test app running on -'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'];
 //phpinfo();
 ?>
